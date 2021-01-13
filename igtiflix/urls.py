9f413806fa0a4f16b7850a066cef6de1d1130636
Daniel Viana <daniel.viana.12@hotmail.com>
2021-01-13 20:32:45 -0300

    Projeto Final - v1.

--- igtiflix/urls.py
+++ igtiflix/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(route='principal/',view=include('principal.urls')),
-    path(route=r'genero/',view=include('genero.urls'))
+    path(route='genero/',view=include('genero.urls')),
+    path(route='serie/',view=include('serie.urls')),
+
 ]
